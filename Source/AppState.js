5165f0ece6e2ec7d050719d0108368ab3d17112a
Ronald43 <Ronald999998@gmail.com>
2020-07-23 22:47:05 +0200

    Added some hopefully helpful comments

--- Source/AppState.js
+++ Source/AppState.js
@@ -1,19 +1,25 @@
+
+// A utility class used mostly by other classes to modify and access the state of the app 
 class AppState
 {
 
+  // Get the board that is currently being viewed/edited
   static GetCurrentBoard()
   {
     var id = parseInt(localStorage.getItem("currentBoard"))
     return this.boards[id]
   }
 
+  // Set the board that is being viewed/edited
   static SetCurrentBoardId(value)
   {
     localStorage.setItem("currentBoard", value)
   }
 
+  // Return the total number of saved boards the user owns
   static GetBoardsCount()
   {
+    // load the list of boards from locaStarage
     var savedBoards = JSON.parse(localStorage.getItem("Boards"));
     
     if(savedBoards == null){
@@ -23,21 +29,25 @@ class AppState
     return savedBoards.length;
   }
 
+  // An array of all the boards the user owns 
+  // NOTE: initialize the array with a "null" board so code completion works (I can't work without)
   static boards = [new Board(-1,"null",[])];
 
+  // Get the array of all the boards the user owns
   static GetBoards()
   {
+    // if the array is "empty"
     if(this.boards[0].id == -1)
     {
-      this.Load()
+      this.Load() //Load them from locaStorage
     }
     
     return this.boards;
   }
 
+  // Delete a board by its id
   static DeleteBoard(id)
   {
-    // NOTE: Could this be optimized somehow?
 
     // Delete the board on the boards list by
     // filtering for all the boards that don't have that id
@@ -45,6 +55,7 @@ class AppState
     this.boards = this.boards.filter(b => b.id != id)
     
     // Reassign the id's of every board that was affected by the deletions
+    // TODO: Could this be optimized somehow?
     for (let i = id; i < this.boards.length; i++) {
       const b = this.boards[i];
       b.id = i
